81229373112fd58c5d48298f6d05870eb3433f32
mrpumpkin98 <sju4486@naver.com>
2023-06-26 14:29:14 +0900

    week : all

--- pages/Week4/06/06-01.jsx
+++ pages/Week4/06/06-01.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
+import { Modal } from "antd";
 
 export default function Weather() {
   const [movies, setMovies] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false); // 모달의 가시성 상태
+  const [selectedMovie, setSelectedMovie] = useState(null); // 선택한 영화
+
   const API_KEY = "15f8fbf5168d6da001f1e3c2c4b76277";
 
   useEffect(() => {
@@ -13,20 +17,56 @@ export default function Weather() {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    gap: 10px; /* 열 사이의 간격 설정 */
-    width: 50%;
-    margin: 0 auto; /* 가운데 정렬 */
+    gap: 10px;
+    width: 80%;
+    margin: 0 auto;
+    padding: 40px 0px;
   `;
 
   const Item = styled.div`
-    width: calc(30% - 20px); /* 한 줄에 3개의 아이템 배치 (간격 제외) */
+    width: calc(25% - 20px);
     text-align: center;
   `;
 
   const Img = styled.img`
     cursor: pointer;
-    background-color: gray; /* 이미지가 없을 때의 대체 배경색 */
+    background-color: white;
     object-fit: cover;
+    transition: border-color 0.3s, transform 0.3s; /* 테두리 색상 및 변화에 애니메이션 효과 추가 */
+    border: 2px solid transparent; /* 초기 테두리 설정 */
+    margin-top: 20px;
+    :hover {
+      transform: translateY(-10px); /* 위로 10px 이동하는 애니메이션 효과 */
+    }
+  `;
+
+  const Tie = styled.div`
+    display: flex;
+  `;
+
+  const TextTie = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+  `;
+
+  const P1 = styled.p`
+    margin-top: 10px;
+  `;
+
+  const Button = styled.button`
+    margin-top: 10px;
+    cursor: pointer;
+    background-color: #1890ff;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    border-radius: 4px;
+    transition: background-color 0.3s;
+    margin-top: 30px;
+    :hover {
+      background-color: #40a9ff;
+    }
   `;
 
   const fetchMoviesData = async () => {
@@ -35,15 +75,33 @@ export default function Weather() {
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&api_key=${API_KEY}`
       );
       const data = await response.json();
-      console.log(data.data.movies); // API 값을 콘솔에 출력
+      console.log(data.data.page_number);
       setMovies(data.data.movies);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleMovieClick = (url) => {
-    window.open(url, "_blank"); // 새 창에서 해당 URL 열기
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
+  const truncateSummary = (summary, maxLength) => {
+    if (summary.length > maxLength) {
+      return summary.substring(0, maxLength) + "...";
+    }
+    return summary;
+  };
+
+  const handleMoreButtonClick = () => {
+    if (selectedMovie && selectedMovie.url) {
+      window.open(selectedMovie.url, "_blank");
+    }
   };
 
   return (
@@ -54,14 +112,33 @@ export default function Weather() {
             src={movie.medium_cover_image}
             alt={movie.title}
             onError={(event) => {
-              event.target.style.display = "none"; // 이미지 로딩 실패 시 숨기기
+              event.target.style.display = "none";
             }}
-            onClick={() => handleMovieClick(movie.url)}
+            onClick={() => handleMovieClick(movie)}
           />
-          <p>제목: {movie.title}</p>
-          <p>연도: {movie.year}</p>
         </Item>
       ))}
+      {selectedMovie && (
+        <Modal
+          visible={modalVisible}
+          onCancel={handleModalClose}
+          footer={null}
+          destroyOnClose
+        >
+          <Tie>
+            <img
+              src={selectedMovie.medium_cover_image}
+              alt={selectedMovie.title}
+            />
+            <TextTie>
+              <P1>제목: {selectedMovie.title}</P1>
+              <P1>연도: {selectedMovie.year}</P1>
+              <P1>내용: {truncateSummary(selectedMovie.summary, 200)}</P1>
+              <Button onClick={handleMoreButtonClick}>더보기</Button>
+            </TextTie>
+          </Tie>
+        </Modal>
+      )}
     </Main>
   );
 }
